1191ce28fdb7f9d9d31274a96e4c8904abd690d0
softbenz infosys <sahjk715@gmail.com>
2020-05-06 08:53:21 +0545

    Write Custom Middleware and use in nodejs

--- expressjs/main.js
+++ expressjs/main.js
@@ -1,6 +1,35 @@
 const express =require("express");
 const app =express();
+var myMiddleware = require('./my-middleware.js')
 app.use('/abc', express.static('public'));
+
+var Validation= function(req,res,next){
+    console.log('Middleware Working');
+    next();
+}
+
+var userValidation= function(req,res,next){
+    if(req.params.username =='jitendra')
+    console.log('User Validate ');
+    else
+    console.log("Not Authorized User");
+    next();
+}
+
+// app.use(Validation);
+
+app.use(myMiddleware({ option1: '1', option2: '2' }));
+app.use(userValidation);
+app.get('/',Validation,(req,res)=>{
+    res.send("Hello world in nepal");
+});
+
+app.get('/users/:username',(req,res)=>{
+    res.send("Users Profile");
+});
+
+
+/*
 app.get('/ab(*)cd/',(req, res)=>{
     console.log(req.params);
     res.send('Hello World: ');
@@ -23,5 +52,6 @@ app.get("/flights/:From?.:To?",(req, res)=>{
 app.post("/users/profile",(req, res)=>{
     res.send("Users Profile Data accessed");
 });
+*/
 
 app.listen(3000,()=>console.log("Server running on port:3000"));
