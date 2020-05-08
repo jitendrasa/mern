baaa00b51da8d88cfe8c4273e5280e64d3b6b004
softbenz infosys <sahjk715@gmail.com>
2020-05-08 09:07:07 +0545

    use different engine in node js

--- expressjs/main.js
+++ expressjs/main.js
@@ -1,57 +1,13 @@
 const express =require("express");
 const app =express();
-var myMiddleware = require('./my-middleware.js')
 app.use('/abc', express.static('public'));
+app.set('view engine', 'twig');
+app.set('views','./public/views');
 
-var Validation= function(req,res,next){
-    console.log('Middleware Working');
-    next();
-}
+app.get('/',(req,res)=>{
 
-var userValidation= function(req,res,next){
-    if(req.params.username =='jitendra')
-    console.log('User Validate ');
-    else
-    console.log("Not Authorized User");
-    next();
-}
-
-// app.use(Validation);
-
-app.use(myMiddleware({ option1: '1', option2: '2' }));
-app.use(userValidation);
-app.get('/',Validation,(req,res)=>{
-    res.send("Hello world in nepal");
+    res.render('index',{title:"Tutorialswebsite",message:'Tutorials website is an learningportal'});
 });
 
-app.get('/users/:username',(req,res)=>{
-    res.send("Users Profile");
-});
-
-
-/*
-app.get('/ab(*)cd/',(req, res)=>{
-    console.log(req.params);
-    res.send('Hello World: ');
-});
- 
-app.get("/users/:Id?",(req, res)=>{
-
-    if(req.params.Id ==undefined)
-        res.send("All Users Data accessed");
-    else
-    res.send("Users Data accessed: "+ req.params.Id);
-});
-
-app.get("/flights/:From?.:To?",(req, res)=>{
-
-    console.log(req.params)
-    res.send("Search for flights: " + "From: "+ req.params.From + " To: "+ req.params.To);
-});
-
-app.post("/users/profile",(req, res)=>{
-    res.send("Users Profile Data accessed");
-});
-*/
 
 app.listen(3000,()=>console.log("Server running on port:3000"));
