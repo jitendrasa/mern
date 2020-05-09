09a551ee7e3246a03708e91d2963519b2ffd71cd
softbenz infosys <sahjk715@gmail.com>
2020-05-09 10:39:39 +0545

    How to handle get and parse post request in express.js

--- expressjs/main.js
+++ expressjs/main.js
@@ -1,13 +1,34 @@
 const express =require("express");
+const bodyParser = require('body-parser');
 const app =express();
 app.use('/abc', express.static('public'));
 app.set('view engine', 'twig');
 app.set('views','./public/views');
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
 app.get('/',(req,res)=>{
 
-    res.render('index',{title:"Tutorialswebsite",message:'Tutorials website is an learningportal'});
+    res.render('index',{title:"Login Form",message:'Enter Username and Password'});
 });
 
+app.post('/',(req,res)=>{
+
+    res.render('login',{title:"User Details",username:req.body.username, password:req.body.password});
+});
+
+app.get('/about/:a-:b',(req,res)=>{
+
+    res.render('about',{title:"About",
+        sum:parseInt(req.params.a) + parseInt(req.params.b), 
+        sub:parseInt(req.params.a) - parseInt(req.params.b), 
+        mul:parseInt(req.params.a) * parseInt(req.params.b), 
+        div:parseInt(req.params.a) / parseInt(req.params.b)});
+});
 
 app.listen(3000,()=>console.log("Server running on port:3000"));
